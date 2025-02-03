6d0baf41a45d4f7a01011bfc1ad2788328e60a7c
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2025-02-03 17:32:46 -0500

    WEBRTC-129 Inbound Sample App Demo

--- fb_helper.js
+++
@@ -1,5 +0,0 @@
-/**Helper for firebase initiation and operations */
-const firebase = require('firebase-admin');
-const serviceAccount = require('./admin-service.json');
-firebase.initializeApp({ credential: firebase.credential.cert(serviceAccount) });
-export { firebase };
